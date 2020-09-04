d01ef8106faa73a3522ea9faebcbfba5a6b17acf
akihiro.tanaka <big.a.0816@gmail.com>
2020-09-05 02:02:49 +0900

    add contract

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# chainlink-node
+# Chainlink Node
 Docker Compose sample for Chainlink Node
 
 ## Getting Started
@@ -26,13 +26,100 @@ $ cp chainlink/.env.sample chainlink/.env
 
 Set the variables `ETH_URL` & `ETH_CHAIN_ID` in the `.env` to your Ethereum client's URL & Chain ID.
 
-## 3. Run a Chainlink Node
+### 3. Run a Chainlink Node
 
 Run the Docker images by running:
 ```bash
 $ docker-compose up -d
-$ docker exec -it -d chainlink chainlink local n -p /chainlink/.password -a /chainlink/.api
+$ docker exec -it chainlink chainlink local n -p /chainlink/.password -a /chainlink/.api
 ```
 
-## 4. Connect to Chainlink node's UI interface
+### 4. Connect to Chainlink node's UI interface
 Open `http://localhost:6688`.
+
+
+# Contract
+
+## Installation
+
+```bash
+npm install
+```
+
+## Test
+
+```bash
+npm test
+```
+
+## Deploy
+
+If needed, edit the `truffle-config.js` config file to set the desired network to a different port. It assumes any network is running the RPC port on 8545.
+
+```bash
+npm run migrate:dev
+```
+
+For deploying to live networks, Truffle will use `truffle-hdwallet-provider` for your mnemonic and an RPC URL. Set your environment variables `$RPC_URL` and `$MNEMONIC` before running:
+
+```bash
+npm run migrate:live
+```
+
+## Helper Scripts
+
+There are 3 helper scripts provided with this box in the scripts directory:
+
+- `fund-contract.js`
+- `request-data.js`
+- `read-contract.js`
+
+They can be used by calling them from `npx truffle exec`, for example:
+
+```bash
+npx truffle exec scripts/fund-contract.js --network live
+```
+
+The CLI will output something similar to the following:
+
+```
+Using network 'live'.
+
+Funding contract: 0x972DB80842Fdaf6015d80954949dBE0A1700705E
+0xd81fcf7bfaf8660149041c823e843f0b2409137a1809a0319d26db9ceaeef650
+Truffle v5.0.25 (core: 5.0.25)
+Node v10.16.3
+```
+
+In the `request-data.js` script, example parameters are provided for you. You can change the oracle address, Job ID, and parameters based on the information available on [our documentation](https://docs.chain.link/docs/testnet-oracles).
+
+```bash
+npx truffle exec scripts/request-data.js --network live
+```
+
+This creates a request and will return the transaction ID, for example:
+
+```
+Using network 'live'.
+
+Creating request on contract: 0x972DB80842Fdaf6015d80954949dBE0A1700705E
+0x828f256109f22087b0804a4d1a5c25e8ce9e5ac4bbc777b5715f5f9e5b181a4b
+Truffle v5.0.25 (core: 5.0.25)
+Node v10.16.3
+```
+
+After creating a request on a live network, you will want to wait 3 blocks for the Chainlink node to respond. Then call the `read-contract.js` script to read the contract's state.
+
+```bash
+npx truffle exec scripts/read-contract.js --network live
+```
+
+Once the oracle has responded, you will receive a value similar to the one below:
+
+```
+Using network 'live'.
+
+21568
+Truffle v5.0.25 (core: 5.0.25)
+Node v10.16.3
+```
---
+++ contracts/WebScrapingData.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.9;
+
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+/**
+ * @title WebScrapingData is an example contract which requests data from
+ * the Chainlink network
+ * @dev This contract is designed to work on multiple networks, including
+ * local test networks
+ */
+contract WebScrapingData is ChainlinkClient, Ownable {
+  bytes32 public data;
+  string public url;
+
+  /**
+   * @notice Deploy the contract with a specified address for the LINK
+   * and Oracle contract addresses
+   * @dev Sets the storage for the specified addresses
+   * @param _link The address of the LINK token contract
+   */
+  constructor(address _link) public {
+    if (_link == address(0)) {
+      setPublicChainlinkToken();
+    } else {
+      setChainlinkToken(_link);
+    }
+  }
+
+  function setUrl(string memory _url) public onlyOwner {
+    url = _url;
+  }
+
+  /**
+   * @notice Returns the address of the LINK token
+   * @dev This is the public implementation for chainlinkTokenAddress, which is
+   * an internal method of the ChainlinkClient contract
+   */
+  function getChainlinkToken() public view returns (address) {
+    return chainlinkTokenAddress();
+  }
+
+  /**
+   * @notice Creates a request to the specified Oracle contract address
+   * @dev This function ignores the stored Oracle contract address and
+   * will instead send the request to the address specified
+   * @param _oracle The Oracle contract address to send the request to
+   * @param _jobId The bytes32 JobID to be executed
+   * @param _path The dot-delimited path to parse of the response
+   * @param _filter The filter string to response data
+   */
+  function createRequestTo(
+    address _oracle,
+    bytes32 _jobId,
+    uint256 _payment,
+    string memory _path,
+    string memory _index,
+    string memory _filter
+  )
+    public
+    onlyOwner
+    returns (bytes32 requestId)
+  {
+    require(bytes(url).length > 0, "url is required");
+    Chainlink.Request memory req = buildChainlinkRequest(_jobId, address(this), this.fulfill.selector);
+    req.add("url", url);
+    req.add("path", _path);
+    req.add("index", _index);
+    req.add("filter", _filter);
+    requestId = sendChainlinkRequestTo(_oracle, req, _payment);
+  }
+
+  /**
+   * @notice The fulfill method from requests created by this contract
+   * @dev The recordChainlinkFulfillment protects this function from being called
+   * by anyone other than the oracle address that the request was sent to
+   * @param _requestId The ID that was generated for the request
+   * @param _data The answer provided by the oracle
+   */
+  function fulfill(bytes32 _requestId, bytes32 _data)
+    public
+    recordChainlinkFulfillment(_requestId)
+  {
+    data = _data;
+  }
+
+  /**
+   * @notice Allows the owner to withdraw any LINK balance on the contract
+   */
+  function withdrawLink() public onlyOwner {
+    LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+    require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
+  }
+
+  /**
+   * @notice Call this method if no response is received within 5 minutes
+   * @param _requestId The ID that was generated for the request to cancel
+   * @param _payment The payment specified for the request to cancel
+   * @param _callbackFunctionId The bytes4 callback function ID specified for
+   * the request to cancel
+   * @param _expiration The expiration generated for the request to cancel
+   */
+  function cancelRequest(
+    bytes32 _requestId,
+    uint256 _payment,
+    bytes4 _callbackFunctionId,
+    uint256 _expiration
+  )
+    public
+    onlyOwner
+  {
+    cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
+  }
+}
